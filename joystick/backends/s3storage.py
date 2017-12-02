a3c74a42440e679f8efbf9c441eaefc5b159c240
SergeiPogorelov <pogorelov.ss@gmail.com>
2017-12-02 18:59:14 +0200

    add task and init model

--- joystick/backends/s3storage.py
+++ joystick/backends/s3storage.py
@@ -5,5 +5,6 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class StaticStorage(S3Boto3Storage):
     location = settings.AWS_STATICFILES_LOCATION
 
+
 class MediaStorage(S3Boto3Storage):
     location = settings.AWS_MEDIAFILES_LOCATION
